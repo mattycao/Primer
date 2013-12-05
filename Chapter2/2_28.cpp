09f492aacc677d1fcf8cf73aac74c01a4261511f
MattCao <mattycao@gmail.com>
2013-12-04 21:25:53 -0800

    finish the chapter 2

---
+++ Chapter2/2_28.cpp
@@ -0,0 +1,9 @@
+class Foo {
+	//empty
+}; // test for the missing semicolon
+int main() {
+	return 0;
+}
+// After do the missing semicolon test, the complier gives me an error that:
+	// 1 error generated.
+	// so good for debug, a good complier
